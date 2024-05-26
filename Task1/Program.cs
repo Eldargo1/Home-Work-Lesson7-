5c232a275581d5161ef5e846c1f6a36cb85884b4
Eldar <kastoom204@gmail.com>
2024-05-26 19:38:27 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу,  
+//которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+Console.WriteLine("Введите число M");
+int userFirstNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int userSecondnum = int.Parse(Console.ReadLine());
+
+SetSumNum(userFirstNum, userSecondnum);
+
+void SetSumNum(int start, int end)
+{
+    if(start > end)
+    {
+        return;
+    }
+
+    Console.Write(start+ " ");
+    start++;
+    SetSumNum(start, end);
+}
